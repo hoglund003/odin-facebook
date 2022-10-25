31e09e12be6534987bbf51f802a28b7e4bb08e8d
Kasper Høglund <kasper@rubynor.no>
2022-10-25 10:37:58 +0200

    User model added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.delete_all
+
+password = "Admin:123"
+User.create!(email: "admin@local", password: password)
+10.times do 
+  email = Faker::Internet.safe_email
+  User.create!(email: email, password: password)
+end
