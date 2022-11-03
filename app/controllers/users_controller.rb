8853cc0284d2decb528c0dc76d04a796d6ce986c
Kasper Høglund <kasper@rubynor.no>
2022-11-03 14:08:24 +0100

    display friends added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   def show
-    if params[:id].nil?
-      @user = current_user
+    @user = find_user(params[:id])
+  end
+
+  def friends
+    @user = find_user(params[:user_id])
+    @friends = @user.friends
+  end
+
+  private
+
+  def find_user(id)
+    # Finds correct user when "/profile" is called
+    if id.nil?
+      current_user
     else
-      @user = User.find(params[:id])
+      User.find(id)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, path: 'u'
   resources :users, only: ['show'] do 
     resource :friend_request, only: ['create']
+    get "friends" => "users#friends"
   end
   get "profile" => "users#show"
+  get "profile/friends" => "users#friends"
   resources :friend_requests, only: ['update']
   root 'static_pages#home'
 end
