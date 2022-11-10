7fcab50efabc9ba5e70ea9ad7886b633e9d7fd0d
kasper_hoglund <kasper@rubynor.com>
2022-11-10 10:12:19 +0100

    "jobs" -> "job"

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,10 +3,10 @@ class Profile < ApplicationRecord
   has_one :biography
   has_one :profile_town
   has_one :town, through: :profile_town
-  has_many :jobs
+  has_one :job
   has_one :relationship
 
-  accepts_nested_attributes_for :user, :biography, :town, :jobs, :relationship
+  accepts_nested_attributes_for :user, :biography, :town, :job, :relationship
 
   def full_name
     "#{first_name} #{last_name}"
