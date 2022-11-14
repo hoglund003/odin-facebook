dc4fa6978ccdc1fc3dbae45f436526c7362a9c7d
kasper_hoglund <kasper@rubynor.com>
2022-11-14 15:45:11 +0100

    display bio on profile page as edit form

--- app/controllers/biography_controller.rb
+++ app/controllers/biography_controller.rb
@@ -1,2 +1,25 @@
 class BiographyController < ApplicationController
+  def edit
+    @user = find_user(params[:id])
+
+    if turbo_frame_request?
+      render partial: "profile/biography", locals: { biography: @biography }
+      return
+    end
+  end
+
+  def update
+
+  end
+
+  private
+
+  def find_user(id)
+    # Finds correct user when "/profile" is called
+    if id.nil?
+      current_user
+    else
+      User.find(id)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   post "profile/new" => "profile#create"
   get "profile/edit" => "profile#edit"
   post "profile/edit" => "profile#update"
-  get "profile/bio" => "biography#show"
+  get "profile/bio" => "biography#edit"
+  post "profile/bio" => "biography#update"
   resources :friend_requests, only: ['update', 'destroy']
   resources :posts, only: ['index', 'show', 'new', 'create'] do 
     resource :like, only: ['create', 'destroy']
