3bc19c1196fb84b2a5b6fc7cb3a13b901335aa9b
kasper_hoglund <kasper@rubynor.com>
2022-11-14 15:08:49 +0100

    biography_controller added

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post "profile/new" => "profile#create"
   get "profile/edit" => "profile#edit"
   post "profile/edit" => "profile#update"
+  get "profile/bio" => "biography#show"
   resources :friend_requests, only: ['update', 'destroy']
   resources :posts, only: ['index', 'show', 'new', 'create'] do 
     resource :like, only: ['create', 'destroy']
