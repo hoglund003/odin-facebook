27bb0db92be5e403dd2b28f93753918cb075a1e4
kasper_hoglund <kasper@rubynor.com>
2022-11-15 11:24:09 +0100

    create user from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,12 @@ class User < ApplicationRecord
   def friend_requests_out
     self.friend_requests
   end
+
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.build_profile(first_name: auth.info.name)
+    end
+  end
 end
